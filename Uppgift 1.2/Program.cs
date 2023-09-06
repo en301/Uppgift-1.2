499c7fda99dc91e2ad6cb3b8b54e2c0aae89fae4
emsj06001 <emsj06001@E-PW024NWP>
2023-09-06 10:40:01 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift_1_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ett allvarligt fel har upptäckts i din dator... Den är full av vatten vänligen stäng av den omgående och starta den aldrig igen.");
+            Console.ReadKey();
+        }
+    }
+}
